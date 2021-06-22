b0e34be1f03ad4c78b66ec122958b585dda90533
vLonac <vanjalonac13@gmail.com>
2021-06-22 14:04:47 +0200

    TCP u main folderu
    Dodan modul tcp user logging servera u direktorij glavnog programa

---
+++ TCP User Log Server/TCP.cpp
@@ -0,0 +1,23 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "TCP.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm1 *Form1;
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::IdTCPServer1Execute(TIdContext *AContext)
+{
+	String connection = AContext->Connection->Socket->ReadLn();
+	ListBoxLog->Items->Add(connection);
+	AContext->Connection->Disconnect();
+}
+//---------------------------------------------------------------------------
